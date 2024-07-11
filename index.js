16584ba1422c4303b3e35d886f9b34b1e9be1eb4
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2024-07-11 14:17:19 +0530

    added env variables

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const server = http.createServer(function (request, response) {
     const today = getToday();
     console.log("today", today);
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("Today is: " + today);
+
+    response.end("Today is: " + today + "\n\n" + process.env.HOSTNAME);
   }
 });
 
 const port = 3000;
 const host = "127.0.0.1";
 server.listen(port, host);
-console.log(`Listening at http://${host}:${port}`);
+console.log(
+  `Listening at http://${host}:${port}`,
+  `|| Hostname - ${process.env.HOSTNAME}`
+);
